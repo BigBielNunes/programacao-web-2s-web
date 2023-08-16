ec48f1bf755363a8c3d616e8a4c04a09247f90a4
BigBielNunes <bielnsm@gmail.com>
2023-08-15 21:00:18 -0300

    Atividade 1 extensão programação para web faculdade

---
+++ variaveisOperadores.js
@@ -0,0 +1,13 @@
+var num_A = 6
+var num_B = 3
+
+var soma = num_A + num_B
+
+var sub = num_A - num_B
+
+
+var mult = num_A * num_B
+
+var div = num_A/num_B
+
+console.log(`Resultado da Soma entre ${num_A} e ${num_B} = ${soma}\nResultado da subtração entre ${num_A} e ${num_B} = ${sub}\nResultado da multiplicação entre ${num_A} e ${num_B} = ${mult}\nResultado da divisão entre ${num_A} e ${num_B} = ${div}`)
